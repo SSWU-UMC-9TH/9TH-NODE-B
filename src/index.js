7c48e35f6d8e01a0eae73faa4b617fc03176f658
rimit-rim <yeom.surim@gmail.com>
2025-11-11 23:27:58 +0900

    [FEAT] Swagger 설정하기

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { StatusCodes } from "http-status-codes";
 import morgan from 'morgan';
 import cookieParser from "cookie-parser";
 import compression from "compression";
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 
 import { handleUserSignUp } from "./controllers/user.controller.js";
 import { handleCreateStore, handleListStoreReviews } from "./controllers/store.controller.js";
@@ -102,6 +104,38 @@ const isLogin = (req, res, next) => {
         res.status(401).send('<script>alert("로그인이 필요합니다!");location.href="/login";</script>');
     }
 };
+
+// swagger 설정
+app.use(
+    "/docs",
+    swaggerUiExpress.serve,
+    swaggerUiExpress.setup({}, {
+        swaggerOptions: {
+            url: "/openapi.json",
+        },
+    })
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+    // #swagger.ignore = true
+    const options = {
+        openapi: "3.0.0",
+        disableLogs: true,
+        writeOutputFile: false,
+    };
+    const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+    const routes = ["./src/index.js"];
+    const doc = {
+        info: {
+            title: "UMC 9th",
+            description: "UMC 9th Node.js 테스트 프로젝트입니다.",
+        },
+        host: "localhost:3000",
+    };
+
+    const result = await swaggerAutogen(options)(outputFile, routes, doc);
+    res.json(result ? result.data : null);
+});
 
 
 app.get('/', (req, res) => {
